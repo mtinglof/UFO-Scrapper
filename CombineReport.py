2d1abf43bf8919042f1f92e1fc3d49c87804958c
Michael Tinglof <mike.tinglof@gmail.com>
2024-11-06 15:50:28 -0800

    A little update to the CombineReport file.

--- CombineReport.py
+++ CombineReport.py
@@ -1,10 +1,10 @@
 import pandas
 import os
 
-listed_dates = os.listdir("./to_combine")
+listed_dates = os.listdir("./saved_data")
 df_hold = []
 for data_file in listed_dates:
-    file_path = "./to_combine/" + data_file
+    file_path = "./saved_data/" + data_file
     df_hold.append(pandas.read_csv(file_path))
 df_main = pandas.concat(df_hold)
 df_main.to_csv("combined_reports.csv", index=False)
